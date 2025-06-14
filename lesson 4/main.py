f777e73d2a4212e789071e20b46fb5f4220001c3
Isaac2011-JetLearner <oliveredosa@yahoo.com>
2025-06-14 14:04:51 +0100

    updated

--- lesson 4/main.py
+++ lesson 4/main.py
@@ -1,1 +1,30 @@
-print("hello world")
+import pgzrun
+import random
+
+WIDHT = 500
+HIGHT = 500
+satalites = []
+
+def create_satalites():
+    for i in range(8):
+        sat = Actor("satalite")
+        satalites.append(sat)
+        sat.pos = random.randint(50,450),random.randint(50,450)
+
+create_satalites()
+
+def draw():
+    screen.blit("background",(0,0))
+
+    for i in range(8):
+        satalites[i].draw()
+
+def update():
+    pass
+
+
+
+
+
+
+pgzrun.go()
