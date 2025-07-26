c75a873833ca5b92b81fda6a9a581585341a0357
Isaac2011-JetLearner <oliveredosa@yahoo.com>
2025-07-26 14:02:22 +0100

    updated

--- lesson 5/lesson5.py
+++ lesson 5/lesson5.py
@@ -4,26 +4,34 @@ import random
 WIDTH = 800
 HEIGHT = 500
 
-stars = ["red-star.png","green-star.png","orange-star.png","purple-star.png","yellow-star.png","blue-star.png"]
+gameOver = False
+
+stars = ["red-star.png","orange-star.png","purple-star.png","yellow-star.png","blue-star.png"]
 actor_stars = [ ]
 animation = [ ]
-
+selected_stars = []
 level = 1
-gap = WIDTH/(level+2)
 
-for i in range(level):
-    chosen_star = random.choice(stars)
-    star = Actor(chosen_star)
-    actor_stars.append(star)
+def makestars():
+    gap = WIDTH/(level+2)
 
-green_star = Actor("green-star.png")   
+    for i in range(level):
+       chosen_star = random.choice(stars)
+       selected_stars.append(chosen_star)
 
-actor_stars.append(green_star)
+    selected_stars.append("green-star.png")
 
-random.shuffle(actor_stars)
+    random.shuffle(selected_stars)
 
-for i in range(len(actor_stars)):
-    actor_stars[i].pos = gap*(i+1),20
+    for i in range(len(selected_stars)):
+        star = Actor(selected_stars[i])
+        actor_stars.append(star)
+        star.pos = gap*(i+1),20
+
+    animate_star()
+
+
+
 
 def animate_star():
     for i in range(len(actor_stars)):
@@ -35,23 +43,41 @@ def stop_animation():
     for ani in animation:
         ani.stop()
 
-animate_star()
+makestars()
 
 def draw():
-    screen.blit("space.png",(0,0))
-    for star in actor_stars:
-        star.draw()
+    if not gameOver:
+        screen.blit("space.png",(0,0))
+        for star in actor_stars:
+            star.draw()
+    else:
+        screen.fill("blue")
+        screen.draw.text("Game Over",center = (WIDTH//2 ,HEIGHT//2),fontsize = 20,color = "white")
 
 
 def on_mouse_down(pos):
-    global animation
+    global animation,selected_stars,level,actor_stars,gameOver
     for i in range(len(actor_stars)):
         if actor_stars[i].collidepoint(pos):
-            stop_animation()
-            animation = [ ]
-            for i in range(len(actor_stars)):
-                actor_stars[i].pos = gap*(i+1),20
-            animate_star()
+            if selected_stars[i] == "green-star.png":
+                level+=1
+                stop_animation()
+                animation = []
+                actor_stars = []
+                selected_stars = []
+                makestars()
+
+            else:
+                gameOver = True
+
+
+
+
+            # stop_animation()
+            # animation = [ ]
+            # for i in range(len(actor_stars)):
+            #     actor_stars[i].pos = gap*(i+1),20
+            # animate_star()
             
         
 
